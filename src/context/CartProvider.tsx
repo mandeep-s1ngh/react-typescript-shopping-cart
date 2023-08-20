e346f82c3a59a5627fd9214b4aae23d425510613
Mandeep Singh <92678152+mandeep-s1ngh@users.noreply.github.com>
2023-08-20 18:52:29 +0100

    setup custom hook for shopping cart, create the context for the cart, finish off the cart provider

--- src/context/CartProvider.tsx
+++ src/context/CartProvider.tsx
@@ -1,3 +1,5 @@
+import { useReducer, useMemo, createContext, ReactElement } from "react";
+
 export type CartItemType = {
   sku: string;
   name: string;
@@ -94,3 +96,57 @@ const reducer = (
       throw new Error("Unidentified reducer action type");
   }
 };
+
+const useCartContext = (initCartState: CartStateType) => {
+  const [state, dispatch] = useReducer(reducer, initCartState);
+
+  const REDUCER_ACTIONS = useMemo(() => {
+    return REDUCER_ACTION_TYPE;
+  }, []);
+
+  const totalItems = state.cart.reduce((previousValue, cartItem) => {
+    return previousValue + cartItem.qty;
+  }, 0);
+
+  const totalPrice = new Intl.NumberFormat("en-GB", {
+    style: "currency",
+    currency: "GBP",
+  }).format(
+    state.cart.reduce((previousValue, cartItem) => {
+      return previousValue + cartItem.qty * cartItem.price;
+    }, 0)
+  );
+
+  const cart = state.cart.sort((a, b) => {
+    const itemA = Number(a.sku.slice(-4));
+    const itemB = Number(b.sku.slice(-4));
+    return itemA - itemB;
+  });
+
+  return { dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart };
+};
+
+export type UseCartContextType = ReturnType<typeof useCartContext>;
+
+const initCartContextState: UseCartContextType = {
+  dispatch: () => {},
+  REDUCER_ACTIONS: REDUCER_ACTION_TYPE,
+  totalItems: 0,
+  totalPrice: "",
+  cart: [],
+};
+
+export const CartContext =
+  createContext<UseCartContextType>(initCartContextState);
+
+type ChildrenType = { children?: ReactElement | ReactElement[] };
+
+export const CartProvider = ({ children }: ChildrenType): ReactElement => {
+  return (
+    <CartContext.Provider value={useCartContext(initCartState)}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartContext;
