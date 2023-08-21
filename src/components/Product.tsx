dc4b42428527fa5c9cee81d884bc73a565b1bd07
Mandeep Singh <92678152+mandeep-s1ngh@users.noreply.github.com>
2023-08-21 22:22:37 +0100

    finish off the ProductList component, add and build up the Product component

---
+++ src/components/Product.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement } from "react";
+import { ProductType } from "../context/ProductsProvider";
+import { ReducerActionType, ReducerAction } from "../context/CartProvider";
+
+type PropsType = {
+  product: ProductType;
+  dispatch: React.Dispatch<ReducerAction>;
+  REDUCER_ACTIONS: ReducerActionType;
+  inCart: boolean;
+};
+
+const Product = ({
+  product,
+  dispatch,
+  REDUCER_ACTIONS,
+  inCart,
+}: PropsType): ReactElement => {
+  const img: string = new URL(`../images/${product.sku}.jpg`, import.meta.url)
+    .href;
+  console.log(img);
+
+  return <div>Product</div>;
+};
+
+export default Product;
