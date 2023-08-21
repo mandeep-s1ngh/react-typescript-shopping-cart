e08c143c91aa636f0c3f55c6a39d7171b02cf93f
Mandeep Singh <92678152+mandeep-s1ngh@users.noreply.github.com>
2023-08-21 22:15:39 +0100

    build up the Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import useCart from "../hooks/useCart";
 
-const Footer = () => {
-  return <div>Footer</div>;
+type PropsType = {
+  viewCart: boolean;
+};
+
+const Footer = ({ viewCart }: PropsType) => {
+  const { totalItems, totalPrice } = useCart();
+
+  const year: number = new Date().getFullYear();
+
+  const pageContent = viewCart ? (
+    <p>Shopping Cart &copy; {year}</p>
+  ) : (
+    <>
+      <p>Total Items: {totalItems}</p>
+      <p>Total Price: {totalPrice}</p>
+      <p>Shopping Cart &copy; {year}</p>
+    </>
+  );
+
+  const content = <footer className="footer">{pageContent}</footer>;
+
+  return content;
 };
 
 export default Footer;
